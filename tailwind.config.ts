a4ca7836b7748088ce4a9d8808cefe505e52a7d8
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-02-25 20:24:32 +1300

    Add Ray White custom colours to Tailwind cfg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+            },
+            colors: {
+                'rw-yellow': '#FFE512',
+                'rw-grey': '#333333',
+                'rw-black': '#171E1A',
+                'rw-lite-grey': '#8A8D8C',
+                'rw-white': '#FFFFFF',
+                'rw-off-white': '#F8F8F9',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
