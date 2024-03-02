b293b5e4bbeb9837d3fad1cd437ed5edb29078bb
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-03-02 16:39:35 +1300

    Add basic navbar and links

---
+++ src/components/navigation/Logo.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+export default function Logo() {
+    return (
+        <div className="text-rw-grey font-extrabold px-2 text-2xl">
+            <Link href="./">Team Turner</Link>
+        </div>
+    );
+}
