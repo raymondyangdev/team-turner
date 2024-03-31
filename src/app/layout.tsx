bf0fbdbbfc64ff303e8a4b6683dcb9a9423930ba
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-03-31 14:17:50 +1300

    Move logo to correct directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 // Component imports
 import Navbar from "@/components/navigation/Navbar";
 import Footer from '@/components/Footer';
+import Hero from '@/components/home/Hero';
 
 export const metadata: Metadata = {
     title: 'Team Turner | Ray White Orewa',
@@ -14,17 +15,18 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-          <body>
-              <Navbar />
-              <main className="flex-1 container">{children}</main>
-              <Footer />
-          </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Navbar />
+                <Hero />
+                <main className="flex-1 container">{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
