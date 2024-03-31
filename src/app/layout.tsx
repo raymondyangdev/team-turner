980546ffb4d0a3a726396a9dec6e3a4a59cb6f49
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-03-31 14:22:28 +1300

    Fix container alignment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 // Component imports
 import Navbar from "@/components/navigation/Navbar";
 import Footer from '@/components/Footer';
-import Hero from '@/components/home/Hero';
 
 export const metadata: Metadata = {
     title: 'Team Turner | Ray White Orewa',
@@ -23,8 +22,7 @@ export default function RootLayout({
         <html lang="en">
             <body>
                 <Navbar />
-                <Hero />
-                <main className="flex-1 container">{children}</main>
+                <main className="flex-1">{children}</main>
                 <Footer />
             </body>
         </html>
--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -7,7 +7,7 @@ import NavMenuMobile from './NavMenuMobile';
 export default function Navbar() {
     return (
         <header className="bg-rw-yellow sticky top-0 z-202 py-6 md:py-8">
-            <nav className="flex md:flex-row container flex-wrap justify-between items-center px-4">
+            <nav className="flex md:flex-row container flex-wrap justify-between items-center">
                 <Logo />
                 <NavMenuDesktop />
                 <NavMenuMobile />
